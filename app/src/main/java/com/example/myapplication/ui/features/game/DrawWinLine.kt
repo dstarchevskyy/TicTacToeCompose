0649cea17ba9ad048c6dc6a121afc198418044ae
Dmitry Starchevsky <dstarchevskiy@kamaz.digital>
2024-09-15 16:48:18 +0300

    getSecondaryDiagonalWinData

--- app/src/main/java/com/example/myapplication/ui/features/game/DrawWinLine.kt
+++ app/src/main/java/com/example/myapplication/ui/features/game/DrawWinLine.kt
@@ -19,19 +19,39 @@ fun DrawWinLine(
 
     val density: Int =  configuration.densityDpi
 
-    val screenWidth: Float = (configuration.screenWidthDp * density).toFloat()
-    val screenHeight: Float = (configuration.screenHeightDp * density).toFloat()
+    val screenWidth: Float = (configuration.screenWidthDp.toFloat())// * density).toFloat()
+    val screenHeight: Float = (configuration.screenHeightDp.toFloat())// * density).toFloat()
+
+    println("@@@screenWidth: $screenWidth, screenHeight: $screenHeight")
+
+    println("@@@isWin: ${getWinData(cellsState)}")
+
+    val winData = getWinData(cellsState)
+
+    val startOffset: Offset
+    val endOffset: Offset
+
+    when {
+        winData?.direction == WinData.WinDirection.MAIN_DIAGONAL -> {
+            startOffset = Offset.Zero
+            endOffset = Offset(screenWidth,screenHeight)
+        }
+        winData?.direction == WinData.WinDirection.SECONDARY_DIAGONAL -> {
+            startOffset = Offset(screenWidth,0f)
+//            endOffset = Offset( 0f, screenHeight)
+            endOffset = Offset( 0f, 400f)
+        }
+        else -> return
+    }
 
     Canvas(modifier = Modifier.fillMaxSize()) {
         drawLine(
             color = Color.Cyan,
-            start = Offset.Zero,
-            end = Offset(screenWidth,screenHeight),
+            start = startOffset,
+            end = endOffset,
             strokeWidth = 15f,
         )
     }
-
-    println("@@@isWin: ${getWinData(cellsState)}")
 }
 
 private fun getWinData(cellsState: Map<CellPosition, PlayerSign?>): WinData? {
